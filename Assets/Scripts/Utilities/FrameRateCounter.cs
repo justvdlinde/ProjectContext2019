e04601cbe10c400c24e732ee017d72b5f4885e82
justvdlinde <justvdlinde@hotmail.com>
2019-03-27 14:45:05 +0100

    - ARController edit

---
+++ Assets/Scripts/Utilities/FrameRateCounter.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class FrameRateCounter : MonoBehaviour
+{
+    [SerializeField] private Text frameRateValue;
+    private int fps;
+
+    private void Update() {
+        fps = (int)(1f / Time.unscaledDeltaTime);
+        frameRateValue.text = fps.ToString();
+    }
+}
