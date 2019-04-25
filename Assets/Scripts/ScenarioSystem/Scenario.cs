0b8d8c32796ef0dba5c6c72df3bd1ca5aa635342
JelleDekkers <jelle.dekkers@hotmail.com>
2019-04-25 15:27:13 +0200

    Added enum flag based scenario system

---
+++ Assets/Scripts/ScenarioSystem/Scenario.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scenario : MonoBehaviour
+{
+    public static Action<ScenarioEnumFlags> OnScenarioFinished;
+
+    public ScenarioStatus status;
+    public ScenarioEnumFlags flag;
+    public bool canStart;
+
+    private ScenarioManager scenarioManager;
+
+    private void OnValidate()
+    {
+        name = "Scenario: " + flag;
+
+        scenarioManager = FindObjectOfType<ScenarioManager>();
+    }
+
+    public void UpdateCanStartValue()
+    {
+        // needs to check for lower than flag
+        canStart = scenarioManager.ContainsAllLowerFlags(flag);
+    }
+
+    public void StartScenario()
+    {
+        status = ScenarioStatus.InProgress;
+    }
+
+    public void EndScenario()
+    {
+        status = ScenarioStatus.Completed;
+        OnScenarioFinished?.Invoke(flag);
+    }
+}
---
+++ Assets/Scripts/ScenarioSystem/ScenarioEnumFlags.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+public enum ScenarioEnumFlags
+{
+    None        = (1 << 0),
+    GroteZaal   = (1 << 1),
+    Toren       = (1 << 2),
+    Poort       = (1 << 3)
+}
