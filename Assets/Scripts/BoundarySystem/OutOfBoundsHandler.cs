0833cb5c5e71054de808425de663a8a1af15b00c
JelleDekkers <jelle.dekkers@hotmail.com>
2019-05-15 17:17:04 +0200

    Added out of bounds handler for room 1

---
+++ Assets/Scripts/BoundarySystem/OutOfBoundsHandler.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class OutOfBoundsHandler : MonoBehaviour
+{
+    [SerializeField]
+    private CanvasGroup fadeRoot;
+
+    private void Start()
+    {
+        fadeRoot.alpha = 0;
+    }
+
+    private void OnEnable()
+    {
+        SceneBoundaryTrigger.BoundaryExitEvent += OnBoundaryExitEvent;
+        SceneBoundaryTrigger.BoundaryEnterEvent += OnBoundaryEnterEvent;
+    }
+
+    private void OnDisable()
+    {
+        SceneBoundaryTrigger.BoundaryExitEvent -= OnBoundaryExitEvent;
+        SceneBoundaryTrigger.BoundaryEnterEvent -= OnBoundaryEnterEvent;
+    }
+
+    private void OnBoundaryExitEvent(SceneBoundaryTrigger trigger)
+    {
+        fadeRoot.alpha = 1;
+    }
+
+    private void OnBoundaryEnterEvent(SceneBoundaryTrigger trigger)
+    {
+        fadeRoot.alpha = 0;
+    }
+}
---
+++ Assets/Scripts/BoundarySystem/SceneBoundaryTrigger.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using UnityEngine;
+
+public class SceneBoundaryTrigger : MonoBehaviour
+{
+    public static Action<SceneBoundaryTrigger> BoundaryExitEvent;
+    public static Action<SceneBoundaryTrigger> BoundaryEnterEvent;
+
+    [SerializeField] private bool disableMeshAtStart = true;
+
+    private void Start()
+    {
+        GetComponent<MeshRenderer>().enabled = !disableMeshAtStart;
+    }
+
+    public void OnTriggerExit(Collider other)
+    {
+        BoundaryExitEvent?.Invoke(this);
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        BoundaryEnterEvent?.Invoke(this);
+    }
+}
