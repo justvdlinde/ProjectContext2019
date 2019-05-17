453e52c092b886bfa370464b55097cf9b1bfe6ee
JelleDekkers <jelle.dekkers@hotmail.com>
2019-05-17 19:06:15 +0200

    Added popup service

---
+++ Assets/Scripts/Services/PopupService.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+namespace ServiceLocatorNamespace
+{
+    public class PopupService : IService
+    {
+        private const string POPUP_FILE_PATH = "Popup";
+
+        public Popup InstantiatePopup(string header, string content, PopupButton.Settings button, params PopupButton.Settings[] extraButtons)
+        {
+            Popup popup = Object.Instantiate(Resources.Load<Popup>(POPUP_FILE_PATH));
+            popup.Setup(header, content, button, extraButtons);
+            return popup;
+        }
+    }
+}
---
+++ Assets/Scripts/UI/Popup/Popup.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using TMPro;
+using UnityEngine;
+
+public class Popup : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI header;
+    [SerializeField] private TextMeshProUGUI content;
+    [SerializeField] private Transform buttonGroup;
+    [SerializeField] private PopupButton button;
+
+    public void Setup(string header, string content, PopupButton.Settings buttonSetting, params PopupButton.Settings[] extraButtonsSettings)
+    {
+        this.header.text = header;
+        this.content.text = content;
+
+        for(int i = 0; i < extraButtonsSettings.Length; i++)
+        {
+            PopupButton newButton = Instantiate(button);
+            newButton.Setup(this, extraButtonsSettings[i]);
+            newButton.transform.SetParent(buttonGroup, false);
+        }
+
+        button.Setup(this, buttonSetting);
+    }
+
+    public void Close()
+    {
+        Destroy(gameObject);
+    }
+}
+
